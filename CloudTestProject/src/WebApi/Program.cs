9adf5cd113ad7d38c32cc9af92d668c63c4818fa
HikariTempest <arwed@gmx.at>
2025-01-03 17:09:28 +0100

    feat: implemented authorization

--- CloudTestProject/src/WebApi/Program.cs
+++ CloudTestProject/src/WebApi/Program.cs
@@ -1,5 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
 using CloudTestProject.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Azure;
+using Microsoft.Identity.Web;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,10 +11,42 @@ builder.Configuration.AddUserSecrets<Program>();
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>();
 builder.Services.AddSingleton<BlobStoragePhotoService>();
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+
+// This is required to be instantiated before the OpenIdConnectOptions starts getting configured.
+// By default, the claims mapping will map claim names in the old format to accommodate older SAML applications.
+// For instance, 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role' instead of 'roles' claim.
+// This flag ensures that the ClaimsIdentity claims collection will be built from the claims in the token
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+// Adds Microsoft Identity platform (AAD v2.0) support to protect this Api
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(
+        options => 
+        {
+            builder.Configuration.Bind("AzureAdB2C", options);
+        },
+        options =>
+        {
+            builder.Configuration.Bind("AzureAdB2C", options);
+        });
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll", 
+        b =>
+        {
+            b
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddControllers();
 
 var storageConnection = builder.Configuration["ConnectionStrings:BlobStorageDefault"];
 
@@ -29,9 +64,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// use Cors
+app.UseCors("AllowAll");
+
+app.UseHsts();
+
 app.MapGet("hello", () => "Hello World!");
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
